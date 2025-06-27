aae8430f8e339934e6c86ba5ef5a291d8a15630e
conradorg <you@example.com>
2025-06-27 09:58:27 +0200

    displaying user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Button,
+  Heading,
+  Flex,
+  View,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import "./App.css";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
 
-function App() {
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
+
+export default function App() {
+  // counters
   const [count, setCount] = useState(0)
   const [prime, setPrime] = useState(2)
 
+  // user profile
+  const [userprofiles, setUserProfiles] = useState([]);
+  const { signOut } = useAuthenticator((context) => [context.user]);
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
+  async function fetchUserProfile() {
+    const { data: profiles } = await client.models.UserProfile.list();
+    setUserProfiles(profiles);
+  }
+
   return (
-    <>
+    <Flex
+      className="App"
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      width="70%"
+      margin="0 auto"
+    >
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,23 +60,54 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        There are two buttons, one for counting and one for finding the next prime number. <br />
+        Try it out!
+      </p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h2>Next Prime Number</h2>
-        <button onClick={() => setPrime(nextPrimeNumber(prime))}>
-          prime is {prime}
-        </button>
-    </>
-  )
+      <button onClick={() => setPrime(nextPrimeNumber(prime))}>
+        prime is {prime}
+      </button>
+
+      <br />
+      <Divider />
+
+      <Heading level={1}>My Profile</Heading>
+
+      <Divider />
+
+      <Grid
+        margin="3rem 0"
+        autoFlow="column"
+        justifyContent="center"
+        gap="2rem"
+        alignContent="center"
+      >
+        {userprofiles.map((userprofile) => (
+          <Flex
+            key={userprofile.id || userprofile.email}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="2rem"
+            border="1px solid #ccc"
+            padding="2rem"
+            borderRadius="5%"
+            className="box"
+          >
+            <View>
+              <Heading level="3">{userprofile.email}</Heading>
+            </View>
+          </Flex>
+        ))}
+      </Grid>
+      <Button onClick={signOut}>Sign Out</Button>
+    </Flex>
+  );
 }
 
 function nextPrimeNumber(x) {
@@ -56,5 +129,3 @@ function nextPrimeNumber(x) {
   }
   return x;
 }
-
-export default App
